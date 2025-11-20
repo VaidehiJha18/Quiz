4b832a3a3247806c57acac5c8ca7b55f02768015
Priyanka Gate <priyankagate1703@gmail.com>
2025-11-20 10:55:08 +0530

    addded form changes

--- frontend/src/api/apiService.js
+++ frontend/src/api/apiService.js
@@ -23,3 +23,4 @@ export const fetchQuestions = () => api.get('/questions');
 export const addQuestion = (data) => api.post('/questions', data);
 export const updateQuestion = (id, data) => api.put(`/questions/${id}`, data);
 export const deleteQuestion = (id) => api.delete(`/questions/${id}`);
+
